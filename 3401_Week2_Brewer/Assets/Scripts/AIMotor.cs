a03b2cb1a1d48d739aa26b9489de4c95558c5ba9
codbrew <cb535114@scc-mmlab-13.local>
2019-01-29 16:04:17 -0500

    Week2_WholeProject

---
+++ 3401_Week2_Brewer/Assets/Scripts/AIMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMotor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public NavMeshAgent agent;
+    public Transform destination;
+    public float speed = 25f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.position);
+
+
+    }
+}
